965307c30124cc94d9066e3820858f8a272b004b
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2023-12-31 03:40:10 -0300

    Dar Mode initial config

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -17,9 +17,9 @@ export default function CartPage() {
   if (!isMounted) return null;
 
   return (
-    <div className="bg-white">
+    <div>
       <Container>
-        <div className="px-4 py-16 sm:px-6 lg:px-8">
+        <div className="px-4 py-16 sm:px-6 lg:px-8 ">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
